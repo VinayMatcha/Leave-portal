1970e42c33984011f58c30fabf415f3ca8734ee1
kaushik <venkatakaushik1@gmail.com>
2018-01-15 01:08:58 +0530

    userController

--- src/main/java/vinay/leaveportal/controller/UserController.java
+++ src/main/java/vinay/leaveportal/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import vinay.leaveportal.dao.UserDAO;
 import vinay.leaveportal.entity.Users;
@@ -58,5 +59,13 @@ public class UserController {
         userDAO.delete(user);
         return "redirect:/displayusers";
 
+    }
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public String loginUser(@RequestParam("username") String mail,
+                            @RequestParam("password") String password,
+                            Model model){
+
+        return "emphome";
     }
 }
